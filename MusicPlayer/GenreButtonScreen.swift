0ad1d3969bc671e89e918e591e25c7d2f217b03c
Brent Bumann <brent.bumann@aol.com>
2019-06-24 17:57:58 -0700

    Add play genre method

--- MusicPlayer/GenreButtonScreen.swift
+++ MusicPlayer/GenreButtonScreen.swift
@@ -1,18 +1,35 @@
 import UIKit
+import MediaPlayer
 
 class GenreButtonScreen: UIViewController {
 
+    var musicPlayer = MPMusicPlayerController.applicationMusicPlayer
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     
     @IBAction func GenreButtonTapped(_ sender: UIButton) {
+        playGenre(genre: sender.currentTitle!)
     }
     
     @IBAction func StopButtonTapped(_ sender: UIButton) {
     }
     
     @IBAction func NextButtonTapped(_ sender: Any) {
+    }
+    
+    func playGenre(genre: String){
+        musicPlayer.stop()
+        
+        let query = MPMediaQuery()
+        let predicate = MPMediaPropertyPredicate(value: genre, forProperty: MPMediaItemPropertyGenre)
+        
+        query.addFilterPredicate(predicate)
+        
+        musicPlayer.setQueue(with: query)
+        musicPlayer.shuffleMode = .songs
+        musicPlayer.play()
     }
 }
